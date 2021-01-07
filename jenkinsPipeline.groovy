adaedf710adafceebaefa817386628522bc77b64
aalramadhan <61411466+aalramadhan@users.noreply.github.com>
2021-01-07 11:32:21 +0300

    Update jenkinsPipeline.groovy

--- jenkinsPipeline.groovy
+++ jenkinsPipeline.groovy
@@ -4,14 +4,14 @@ node {
     def mvnHome
 
     environment {
-        GIT_CREDENTIALS_ID = "44bb31cd-b259-4160-a31c-64403b8982d3"
+        GIT_CREDENTIALS_ID = "88c7d5cb-2501-4731-b2fb-1f0a2cbc24b1"
     }
 
     stage('Preparation') {
         git changelog: false,
             credentialsId: '$GIT_CREDENTIALS_ID',
             poll: false,
-            url: 'https://github.com/deveshchanchlani/StudentService.git'
+            url: 'https://github.com/aalramadhan/StudentService.git'
         mvnHome = tool 'maven3'
     }
 
